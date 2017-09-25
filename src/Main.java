f8996c159c05044b6193556d3beeb03a4defbfa0
lucasasia <lucas@asia.edu.tw>
2017-09-25 14:40:55 +0800

    modified

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("106024999 陸清達");// write your code here
+       // System.out.println("資傳系");
     }
 }
